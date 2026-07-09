9d43cc9626202dd2b733b051efe8108de0fb5b01
MD SHAHFAHAD KHAN <168389196+shahfahad09@users.noreply.github.com>
2026-07-09 13:44:04 +0530

    Update chunk_service.py

--- app/services/chunk_service.py
+++ app/services/chunk_service.py
@@ -3,8 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_text_into_chunks(text: str):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200
+        chunk_size=2500,
+        chunk_overlap=300
     )
 
     return splitter.split_text(text)
