061d63a169c19215f2839fbf1dfc78a8ef0f0ae2
shahfahad09 <khannishu522@gmail.com>
2026-07-09 12:32:00 +0530

    ADD procfile for deployment

--- app/config.py
+++ app/config.py
@@ -1,3 +1,41 @@
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# APP_DIR = BASE_DIR / "app"
+# UPLOAD_DIR = BASE_DIR / "uploads"
+# CHROMA_DIR = BASE_DIR / "chroma_db"
+
+# TEMPLATE_DIR = APP_DIR / "templates"
+# STATIC_DIR = APP_DIR / "static"
+
+# UPLOAD_DIR.mkdir(exist_ok=True)
+# CHROMA_DIR.mkdir(exist_ok=True)
+
+# GEMINI_MODEL = "gemini-2.5-flash"
+# EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+
+
+# def read_api_key():
+#     env_path = BASE_DIR / ".env"
+
+#     if not env_path.exists():
+#         raise FileNotFoundError(".env file not found")
+
+#     with open(env_path, "r", encoding="utf-8") as file:
+#         for line in file:
+#             if line.startswith("GEMINI_API_KEY="):
+#                 return line.split("=", 1)[1].strip()
+
+#     return None
+
+
+# GEMINI_API_KEY = read_api_key()
+
+# if not GEMINI_API_KEY:
+#     raise ValueError("GEMINI_API_KEY not found inside .env file.")
+
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,19 +51,19 @@ UPLOAD_DIR.mkdir(exist_ok=True)
 CHROMA_DIR.mkdir(exist_ok=True)
 
 GEMINI_MODEL = "gemini-2.5-flash"
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 
 
 def read_api_key():
+    key = os.getenv("GEMINI_API_KEY")
+    if key:
+        return key
+
     env_path = BASE_DIR / ".env"
-
-    if not env_path.exists():
-        raise FileNotFoundError(".env file not found")
-
-    with open(env_path, "r", encoding="utf-8") as file:
-        for line in file:
-            if line.startswith("GEMINI_API_KEY="):
-                return line.split("=", 1)[1].strip()
+    if env_path.exists():
+        with open(env_path, "r", encoding="utf-8") as file:
+            for line in file:
+                if line.startswith("GEMINI_API_KEY="):
+                    return line.split("=", 1)[1].strip()
 
     return None
 
@@ -33,4 +71,4 @@ def read_api_key():
 GEMINI_API_KEY = read_api_key()
 
 if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY not found inside .env file.")
+    raise ValueError("GEMINI_API_KEY not found.")
